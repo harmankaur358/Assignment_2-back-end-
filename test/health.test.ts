d85a92f9a5d24cfb422dd7c1ed3b3a92cea14fd3
harman kaur <harmankaur2@academic.rrc.ca>
2025-09-24 14:44:28 -0500

    feat: Implemented testing for branch part.

--- test/health.test.ts
+++ test/health.test.ts
@@ -3,7 +3,10 @@ import app from "../src/app";
 
 describe("GET /health", () => {
     it("should return 200 OK", async () => {
+        //Arrange & Act
         const response = await request(app).get("/health");
+
+        //Assert
         expect(response.status).toBe(200);
         expect(response.text).toBe("Server is healthy");
     });
