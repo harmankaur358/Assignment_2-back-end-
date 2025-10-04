d7cce77201d7b0d894be36273c6dafb3d3d6eb50
harman kaur <harmankaur2@academic.rrc.ca>
2025-10-04 13:15:01 -0500

    Final commit of the assignment.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    testMatch: ["**/*.test.ts"],
-    collectCoverageFrom: [
-        "src/**/*.ts",
-        "!src/server.ts", 
-        "!src/types/**/*.ts",
-    ],
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"]
+    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
 };
