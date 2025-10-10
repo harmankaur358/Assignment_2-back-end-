1395ad66c2a8c84b4a71b9fd60453b3ad5d85579
harman kaur <harmankaur2@academic.rrc.ca>
2025-10-09 22:38:53 -0500

    Assignment_3: Created the data model for employees.

---
+++ src/api/v1/models/employeeModel.ts
@@ -0,0 +1,10 @@
+//Employee Model
+export interface Employee{
+    id: number;
+    name: string;
+    position: string;
+    department: string;
+    email: string;
+    phone: string;
+    branchId: number;
+}
