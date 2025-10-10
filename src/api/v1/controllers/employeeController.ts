c1e1bcc7a0c78d7d046c0cec67162522dbfa1376
harman kaur <harmankaur2@academic.rrc.ca>
2025-10-10 13:35:51 -0500

    Assignment_3: Refactored the controllers for employee.

--- src/api/v1/controllers/employeeController.ts
+++ src/api/v1/controllers/employeeController.ts
@@ -1,16 +1,18 @@
 //Import Statements
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import * as employeeService from "../services/employeeService";
+import { successResponse, errorResponse } from "../models/responseModel";
  
 // Create a new employee
-export const createEmployee = (req: Request, res: Response) => {
+export const createEmployee = async(req: Request, res: Response, next: NextFunction) => {
   const { name, position, department, email, phone, branchId } = req.body;
- 
+  try{
   if (!name || !position || !department || !email || !phone || !branchId) {
-    return res.status(400).json({ error: "Missing required fields" });
+    return res.status(400).json(errorResponse("Unable to create employee", "Missing Required fields"
+    ));
   }
  
-  const {message, id} = employeeService.createEmployee({
+  const {message, id} = await employeeService.createEmployee({
     name,
     position,
     department,
@@ -19,75 +21,112 @@ export const createEmployee = (req: Request, res: Response) => {
     branchId,
   });
  
-  res.status(201).json({ message, id });
+  res.status(201).json(successResponse({id},message));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
+
 };
  
 // Get all employees
-export const getAllEmployees = (_: Request, res: Response) => {
-  res.json(employeeService.getAllEmployees());
+export const getAllEmployees = async (_: Request, res: Response, next: NextFunction) => {
+  try {
+  const employees =  await employeeService.getAllEmployees();
+  res.status(200).json(successResponse(employees))
+  }
+  catch(error: unknown) {
+    next(error);
+  }
 };
  
 // Get employee by ID
-export const getEmployeeById = (req: Request, res: Response) => {
+export const getEmployeeById = async(req: Request, res: Response,next: NextFunction) => {
+  try{
   const id = Number(req.params.id);
   if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid employee ID" });
+    return res.status(400).json(errorResponse("Invalid employee id"));
   }
  
-  const employee = employeeService.getEmployeeById(id);
-  if (!employee) return res.status(404).json({ error: "Employee not found" });
+  const employee = await employeeService.getEmployeeById(id);
+  if (!employee) return res.status(404).json(errorResponse("Employee not found"));
  
-  res.json(employee);
+  res.status(200).json(successResponse(employee));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
+
 };
  
 // Update employee
-export const updateEmployee = (req: Request, res: Response) => {
+export const updateEmployee = async(req: Request, res: Response, next: NextFunction) => {
+  try{
   const id = Number(req.params.id);
   if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid employee ID" });
+    return res.status(400).json(errorResponse("Invalid employee id"));
   }
  
-  const result = employeeService.updateEmployee(id, req.body);
+  const result = await employeeService.updateEmployee(id, req.body);
   if (result === "Employee not found") {
-    return res.status(404).json({ error: result });
+    return res.status(404).json(errorResponse(result));
   }
  
-  res.json({ message: result });
+  res.status(200).json(successResponse(result));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
 };
  
 // Delete employee
-export const deleteEmployee = (req: Request, res: Response) => {
+export const deleteEmployee = async (req: Request, res: Response, next: NextFunction) => {
+  try{
   const id = Number(req.params.id);
   if (isNaN(id)) {
-    return res.status(400).json({ error: "Invalid employee ID" });
+    return res.status(400).json(errorResponse("Invalid employee id"));
   }
  
-  const result = employeeService.deleteEmployee(id);
+  const result =  await employeeService.deleteEmployee(id);
   if (result === "Employee not found") {
-    return res.status(404).json({ error: result });
+    return res.status(404).json(errorResponse(result));
   }
  
-  res.json({ message: result });
+  res.json(successResponse(result));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
 };
  
 // Get employees by branch id
-export const getEmployeesByBranch = (req: Request, res: Response) => {
+export const getEmployeesByBranch = async (req: Request, res: Response, next: NextFunction) => {
+  try{
   const branchId = Number(req.params.branchId);
   if (isNaN(branchId)) {
-    return res.status(400).json({ error: "Please provide a valid ID." });
+    return res.status(400).json(errorResponse("Provide a valid id"));
   }
  
-  const finalresult = employeeService.getEmployeesByBranch(branchId);
-  res.status(200).json(finalresult);
+  const finalresult =  await employeeService.getEmployeesByBranch(branchId);
+  res.status(200).json(successResponse(finalresult));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
 };
  
 // Get employees by department
-export const getEmployeesByDepartment = (req: Request, res: Response) => {
+export const getEmployeesByDepartment = async (req: Request, res: Response, next:NextFunction) => {
+  try{
   const { department } = req.params;
   if (!department) {
-    return res.status(400).json({ error: "Department is required field." });
+    return res.status(400).json(errorResponse("Department is required field."));
   }
  
-  const finalresult = employeeService.getEmployeesByDepartment(department);
-  res.status(200).json(finalresult);
+  const finalresult = await employeeService.getEmployeesByDepartment(department);
+  res.status(200).json(successResponse(finalresult));
+  }
+  catch(error: unknown) {
+    next(error);
+  }
 };
